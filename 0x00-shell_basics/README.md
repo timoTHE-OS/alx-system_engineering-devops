68fd71d5344f56cf79cdacb4101a4d2f2729cabe
timoTHE-OS <101520988+timoTHE-OS@users.noreply.github.com>
2022-10-01 05:11:50 +0100

    Add files via upload

--- 0x00-shell_basics/README.md
+++ 0x00-shell_basics/README.md
@@ -1,2 +1,1 @@
-# My Project README file
-# My Project README file
+#SHELL BASIS
---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,16 @@
+# Ussing puppet - intall Web Server Nginx and confugure redirect_me
+# Install Nginx
+exec {'Install_nginx':
+  provider => shell,
+  command  => 'sudo apt-get -y update ;\
+               sudo apt-get -y install nginx ;\
+               sudo chown -R ubuntu /var/www ;\
+               sudo service nginx start',
+}
+# Configure redirect_me
+exec {'Configure_redirect':
+  provider => shell,
+  command  => 'echo "Hello World" > /var/www/html/index.nginx-debian.html ;\
+               sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4 permanent;/" /etc/nginx/sites-available/default ;\
+               sudo service nginx restart',
+}
